999eb62fc2444d6d47dfe54994a13070702552f9
suhyun9764 <suhyun9764@naver.com>
2024-12-11 03:55:05 +0900

    build(): init commit

---
+++ src/main/java/suhyun/duosync/DuoSyncApplication.java
@@ -0,0 +1,13 @@
+package suhyun.duosync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuoSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuoSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/suhyun/duosync/DuoSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package suhyun.duosync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuoSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
